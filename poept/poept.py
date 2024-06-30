b6fa6da03a019e9b4082f5df8e2c2f89674f0bd3
Dzianis <2119348+dzianisv@users.noreply.github.com>
2024-06-30 19:11:02 +0300

    Update selectors

--- poept/poept.py
+++ poept/poept.py
@@ -5,15 +5,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from .tools import find, click, enter, speech, record
+import time
+import logging
 
+logger = logging.getLogger(__name__)
+
+website = "https://poe.com/"
 letters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"]
-
+email_area='input[type="email"]'
+code_area="input[class*=CodeInput"
+go_key='//button[text()="Go"]'
+log_key=f"//button[contains(translate(., '{letters[0]}', '{letters[-1]}' ), 'log')]"
+send_key="button[class*=SendButton]"
+text_area="textarea[class*=TextArea]" 
+clear_key="button[class*=ChatBreak]"
+msg_element="div[class*=Message_botMessageBubble]"
+stop_button_selector="button[class*=ChatStopMessageButton]"
 
 class PoePT:
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument("--log-level=3")
-        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-infobars")
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--disable-notifications")
@@ -22,62 +34,39 @@ class PoePT:
         chrome_service = webdriver.chrome.service.Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
         self.stat = "false"
-        self.prompt = ""
-        self.response = ""
-        self.config()
 
-    def config(self, 
-            website="https://poe.com/",
-            email_key=f"//button[contains(translate(., '{letters[0]}', '{letters[-1]}' ), 'email')]",
-            email_area="input[class*=EmailInput]", 
-            code_area="input[class*=CodeInput",
-            go_key=f"//button[contains(translate(., '{letters[0]}', '{letters[-1]}' ), 'go')]",
-            log_key=f"//button[contains(translate(., '{letters[0]}', '{letters[-1]}' ), 'log')]",
-            talk_key=f"//button[contains(translate(., '{letters[0]}', '{letters[-1]}' ), 'talk')]",
-            send_key="button[class*=SendButton]",
-            text_area="textarea[class*=TextArea]", 
-            clear_key="button[class*=ChatBreak]",
-            msg_element="ChatMessage_messageRow__7yIr2"):
-        self.website = website
-        self.clear_key = clear_key
-        self.code_area = code_area
-        self.talk_key = talk_key
-        self.email_area = email_area
-        self.email_key = email_key
-        self.go_key = go_key
-        self.log_key = log_key
-        self.text_area = text_area
-        self.send_key = send_key
-        self.msg_element = msg_element
+    def getMessage(self):
+        # for i in range(10):
+        #     try:
+        #         self.driver.find_element(By.CSS_SELECTOR, stop_button_selector)
+        #         time.sleep(1)
+        #         continue
+        #     except:
+        #         break
 
-    def getMessage(self, chat_id):
-        self.response = ""
-
-        while True:
+        for i in range(10):
             try:
-                msg = find(self.driver, By.XPATH, f"(//div[@class='{chat_id}'])[last()]")
-                if msg.get_attribute("data-complete") == "false": break
-            except:
-                pass
-    
-        while True:
-            msg = find(self.driver, By.XPATH, f"(//div[@class='{chat_id}'])[last()]")
-            if msg.get_attribute("data-complete") == "true": break
-            self.response = msg.text
-        
-        return self.response
+                elements = self.driver.find_elements(By.CSS_SELECTOR, msg_element)
+                for element in elements:
+                    print(element.text)
+                return elements[-1].text
+            except Exception as e:
+                logger.warning(f"failed to find {msg_element}: {e}")
+                time.sleep(1 * i)
+        else:
+            raise Exception("failed to extract the response message")
     
     def clearcookies(self):
         os.remove("cookies.pkl")
         print("cookies cleared")
     
     def clearchat(self):
-        click(self.driver, By.CSS_SELECTOR, self.clear_key)
+        click(self.driver, By.CSS_SELECTOR, clear_key)
         print("cookies cleared")
 
     def login(self, email):
         try:
-            self.driver.get(self.website)
+            self.driver.get(website)
             self.driver.delete_all_cookies()
             cookies = pickle.load(open("cookies.pkl", "rb"))
             for cookie in cookies:
@@ -85,30 +74,24 @@ class PoePT:
             self.driver.refresh()
 
         except (FileNotFoundError, pickle.UnpicklingError):
-            self.driver.get(self.website)    
+            self.driver.get(website)    
             self.driver.execute_script('window.scrollBy(0, 5);')
             
-            click(self.driver, By.XPATH, self.email_key)
-            enter(self.driver, By.CSS_SELECTOR, self.email_area, email)
-            click(self.driver, By.XPATH, self.go_key)
+            enter(self.driver, By.CSS_SELECTOR, email_area, email)
+            click(self.driver, By.XPATH, go_key)
             
             code = input("Enter code: ")
-            enter(self.driver, By.CSS_SELECTOR, self.code_area, code)
-            click(self.driver, By.XPATH, self.log_key)
+            enter(self.driver, By.CSS_SELECTOR, code_area, code)
+            click(self.driver, By.XPATH, log_key)
 
             pickle.dump(self.driver.get_cookies(), open("cookies.pkl", "wb"))
 
     def ask(self, bot="sage", prompt="hello"):
         self.stat = "wait"
-        self.prompt = prompt
-        
-        self.driver.execute_script(f"window.location.href = '{self.website}{bot}/';")
-        
-        click(self.driver, By.CSS_SELECTOR, self.talk_key)
-        enter(self.driver, By.CSS_SELECTOR, self.text_area, prompt)
-        click(self.driver, By.CSS_SELECTOR, self.send_key)
-
-        text = self.getMessage(self.msg_element)
+        self.driver.execute_script(f"window.location.href = '{website}{bot}/';")
+        enter(self.driver, By.CSS_SELECTOR, text_area, prompt)
+        click(self.driver, By.CSS_SELECTOR, send_key)
+        text = self.getMessage()
         self.stat = "ready"
         return text
     
