83b141c2ac6cc75dd06e35861cd2c7344d96f71f
Mr. Sanchez <mamaexus@gmail.com>
2024-07-21 14:55:39 +0300

    Update voice.py

--- poept/examples/voice.py
+++ poept/examples/voice.py
@@ -1,18 +1,25 @@
 from poept import PoePT
 
 bot = PoePT()
-bot.login("<email>@gmail.com")
+bot.login("<email>")
 
-# speech from mic
-prompt = bot.livevoice(10)
-response = bot.ask("Sage", prompt)
-print("Answer:", response)
+#asking live voice prompt
+while (True):
+    print("Listening...")
+    prompt = bot.live_voice(timeout=4)
+    if(prompt):
+        print("> "+prompt)
+        result = bot.ask(newchat=False, bot="Assistant", prompt=prompt)
+        print(result)
+        if(prompt.__contains__("bye")): break
 
-
-# speech from recorded file
-prompt = bot.filevoice("speech.wav")
-response = bot.ask("Sage", prompt)
-print("Answer:", response)
-
+#asking file voice prompt
+import os
+audio_file = os.path.abspath("audio.wav")
+prompt = bot.file_voice(audio_file)
+if(prompt):
+    print("> "+prompt)
+    result = bot.ask(newchat=False, bot="Assistant", prompt=prompt)
+    print(result)
 
 bot.close()
