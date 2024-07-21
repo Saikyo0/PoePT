ad027d2830677820cd1f33eea1952d1c67d37c20
Mr. Sanchez <mamaexus@gmail.com>
2024-07-21 15:19:00 +0300

    Update text.py

--- poept/examples/text.py
+++ poept/examples/text.py
@@ -6,16 +6,27 @@ bot.login("<email>")
 
 #basic prompt and answer
 prompt = input("> ")
-result = bot.ask("Assistant", prompt)
+result = bot.ask(bot="Assistant", prompt=prompt)
+print("Response:", result)
+
+#basic prompt and answer in the same chat
+prompt = input("> ")
+result = bot.ask(newchat=False, bot="Assistant", prompt=prompt)
 print("Response:", result)
 
 #asking prompt and print while generating answer
-import threading
+prompt = "Write A Lorem Ipsum"
 def ask_bot():
-    bot.ask("Assistant", "Write A Lorem Ipsum")
+    print("> "+prompt)
+    bot.ask(bot="Assistant", prompt=prompt)
 threading.Thread(target=ask_bot).start()
-while bot.stat == "wait":
-    print('\r' + bot.response, end='')
+while True:
+    if bot.prompt == prompt:
+        if bot.status == "wait":
+            print(bot.status)
+            print('\r' + bot.response, end='')
+        elif bot.status == "ready":
+            break
 
 #asking prompt in a loop
 while (True):
@@ -28,5 +39,5 @@ while (True):
 import os
 prompt = input("> ")
 file = os.path.abspath("test.jpg")
-response = bot.ask(newchat=False, bot="Assistant", prompt=prompt, attach_file=file)
+response = bot.ask(bot="Assistant", prompt=prompt, attach_file=file)
 print(response)
